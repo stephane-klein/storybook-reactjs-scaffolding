5fd53a31c2c748cdeee7484b1413b693a9bd05d1
Stéphane Klein <contact@stephane-klein.info>
2019-09-21 14:37:39 +0200

    Remove i18n-sync.js script, use only i18next-scanner #2

--- i18next-scanner.config.js
+++ i18next-scanner.config.js
@@ -1,8 +1,92 @@
-/* eslint-env node */
-/* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
-
 const fs = require('fs');
+const path = require('path');
+
+const _ = require('lodash');
 const chalk = require('chalk');
+const eol = require('eol');
+const VirtualFile = require('vinyl');
+const flattenObjectKeys = require('i18next-scanner/lib/flatten-object-keys')
+    .default;
+const omitEmptyObject = require('i18next-scanner/lib/omit-empty-object')
+    .default;
+
+function getFileJSON(resPath) {
+    try {
+        console.log(fs.realpathSync(path.join('src', 'translations', resPath)));
+        return JSON.parse(
+            fs
+                .readFileSync(fs.realpathSync(path.join('src', 'translations', resPath)))
+                .toString('utf-8')
+        );
+    } catch (e) {
+        console.log(e);
+        return {};
+    }
+}
+
+function flush(done) {
+    const { parser } = this;
+    const { options } = parser;
+
+    // Flush to resource store
+    const resStore = parser.get({ sort: options.sort });
+    const { jsonIndent } = options.resource;
+    const lineEnding = String(options.resource.lineEnding).toLowerCase();
+
+    Object.keys(resStore).forEach(lng => {
+        const namespaces = resStore[lng];
+
+        Object.keys(namespaces).forEach(ns => {
+            let obj = namespaces[ns];
+
+            const resPath = parser.formatResourceSavePath(lng, ns);
+
+            let resContent = getFileJSON(resPath);
+
+            if (options.removeUnusedKeys) {
+                const namespaceKeys = flattenObjectKeys(obj);
+                const resContentKeys = flattenObjectKeys(resContent);
+                const unusedKeys = _.differenceWith(
+                    resContentKeys,
+                    namespaceKeys,
+                    _.isEqual
+                );
+
+                for (let i = 0; i < unusedKeys.length; ++i) {
+                    _.unset(resContent, unusedKeys[i]);
+                }
+
+                resContent = omitEmptyObject(resContent);
+            }
+
+            obj = { ...obj, ...resContent };
+
+            let text = `${JSON.stringify(obj, null, jsonIndent)}\n`;
+
+            if (lineEnding === 'auto') {
+                text = eol.auto(text);
+            } else if (lineEnding === '\r\n' || lineEnding === 'crlf') {
+                text = eol.crlf(text);
+            } else if (lineEnding === '\n' || lineEnding === 'lf') {
+                text = eol.lf(text);
+            } else if (lineEnding === '\r' || lineEnding === 'cr') {
+                text = eol.cr(text);
+            } else {
+                // Defaults to LF
+                text = eol.lf(text);
+            }
+
+            this.push(
+                new VirtualFile({
+                    path: resPath,
+                    contents: Buffer.from(text)
+                })
+            );
+        });
+    });
+
+    done();
+}
 
 module.exports = {
     input: ['src/**/*.js'],
@@ -11,7 +95,7 @@ module.exports = {
         debug: false,
         func: {
             list: ['t'],
-            extensions: ['.js']
+            extensions: ['.js'],
         },
         lngs: ['en', 'fr'],
         ns: ['translation'],
@@ -19,17 +103,16 @@ module.exports = {
         defaultNs: 'translation',
         defaultValue: '__STRING_NOT_TRANSLATED__',
         resource: {
-            loadPath: '{{lng}}/missing/{{ns}}.json',
-            savePath: '{{lng}}/missing/{{ns}}.json',
+            savePath: '{{lng}}/{{ns}}.json',
             jsonIndent: 2,
-            lineEnding: '\n'
+            lineEnding: '\n',
         },
         nsSeparator: false, // namespace separator
         keySeparator: false, // key separator
         interpolation: {
             prefix: '{{',
-            suffix: '}}'
-        }
+            suffix: '}}',
+        },
     },
     transform: function customTransform(file, enc, done) {
         'use strict';
@@ -45,7 +128,7 @@ module.exports = {
                     key,
                     Object.assign({}, options, {
                         nsSeparator: false,
-                        keySeparator: false
+                        keySeparator: false,
                     })
                 );
                 ++count;
@@ -54,12 +137,13 @@ module.exports = {
 
         if (count > 0) {
             console.log(
-                `i18next-scanner: count=${chalk.cyan(count)}, file=${chalk.yellow(
-                    JSON.stringify(file.relative)
-                )}`
+                `i18next-scanner: count=${chalk.cyan(
+                    count
+                )}, file=${chalk.yellow(JSON.stringify(file.relative))}`
             );
         }
 
         done();
-    }
+    },
+    flush
 };
