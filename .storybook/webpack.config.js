b1358388dfd69540cddfe4a5dbead0e830a52478
Stéphane Klein <contact@stephane-klein.info>
2019-09-21 17:24:17 +0200

    Fix addon-storysource configuration error

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,8 +3,8 @@ const path = require("path");
 module.exports = function ({ config }) {
     // This is a workaround https://github.com/storybookjs/storybook/issues/6974#issuecomment-499903328
     config.module.rules.unshift({
-        test: /\.jsx?$/,
-        include: path.resolve(__dirname, "../src/examples"),
+        test: /\.(js|jsx)?$/,
+        include: path.resolve(__dirname, "../stories/"),
         loaders: [
             {
                 loader: require.resolve("@storybook/addon-storysource/loader"),
